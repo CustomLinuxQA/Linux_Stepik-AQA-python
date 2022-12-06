98ec7dd72038f87a7bb4bd0e28ec5f9516751824
goat <customlinuxqa@gmail.com>
2022-12-06 15:16:12 +0300

    refactoring

--- section2/lesson1_step5_checkboxes-and-radiobuttons-click.py
+++ section2/lesson1_step5_checkboxes-and-radiobuttons-click.py
@@ -1,41 +1,29 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-import time, math
+import time
+import math
+import chromedriver_autoinstaller
 
-try:
-    link = "https://suninjuly.github.io/math.html"
-    browser = webdriver.Chrome()
-    browser.get(link)
-
-
-    # Создаем функцию calc(x)
-    def calc(x):
-        return str(math.log(abs(12 * math.sin(int(x)))))
+chromedriver_autoinstaller.install()
+link = "https://suninjuly.github.io/math.html"
 
 
-    # Находим элемент Х
-    x_element = browser.find_element(By.CSS_SELECTOR, ".form-group [id=input_value]")
-    x = x_element.text
-    y = calc(x)
+def calc(x):
+    return str(math.log(abs(12 * math.sin(int(x)))))
 
-    # вводим значение "y" в строку.
-    input1 = browser.find_element(By.ID, "answer")
-    input1.send_keys(y)
 
-    # выбираем чекбокс - im the Robot
-    option1 = browser.find_element(By.ID, "robotCheckbox")
-    option1.click()
+def test_fill_form():
+    try:
+        browser = webdriver.Chrome()
+        browser.get(link)
 
-    # выбираем радиобатон - Robots rule
-    option2 = browser.find_element(By.ID, "robotsRule")
-    option2.click()
+        x_element = browser.find_element(By.CSS_SELECTOR, ".form-group [id=input_value]")
+        x = x_element.text
+        y = calc(x)
 
-    # жмем Submit
-    option3 = browser.find_element(By.CSS_SELECTOR, ".btn")
-    option3.click()
-
-finally:
-    # ожидание чтобы визуально оценить результаты прохождения скрипта
-    time.sleep(10)
-    # закрываем браузер после всех манипуляций
-    browser.quit()
+        browser.find_element(By.ID, "answer").send_keys(y)
+        browser.find_element(By.ID, "robotCheckbox").click()
+        browser.find_element(By.ID, "robotsRule").click()
+        browser.find_element(By.CSS_SELECTOR, ".btn").click()
+    finally:
+        time.sleep(5)
