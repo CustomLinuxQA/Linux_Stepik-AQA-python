0bf2f5b5af9d34eb2411fec0035aa31daa9f31e3
goat <customlinuxqa@gmail.com>
2022-12-06 15:37:34 +0300

    refactoring

--- section2/lesson1_step7_find-with-get_attribute.py
+++ section2/lesson1_step7_find-with-get_attribute.py
@@ -1,44 +1,28 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-import time, math
+import time
+import math
+import chromedriver_autoinstaller
 
-try:
-    link = "http://suninjuly.github.io/get_attribute.html"
-    browser = webdriver.Chrome()
-    browser.get(link)
-
-
-    # Создаем функцию calc(x)
-    def calc(x):
-        return str(math.log(abs(12 * math.sin(int(x)))))
+chromedriver_autoinstaller.install()
+link = "http://suninjuly.github.io/get_attribute.html"
 
 
-    # Находим элемент Х
-    x_element = browser.find_element(By.CSS_SELECTOR, "[valuex]")
+def calc(x):
+    return str(math.log(abs(12 * math.sin(int(x)))))
 
-    # Возьмем значение атрибута "valuex"
-    x_valuex = x_element.get_attribute("valuex")
-    x = x_valuex
-    y = calc(x)
+def test_fill_form():
+    try:
+        browser = webdriver.Chrome()
+        browser.get(link)
 
-    # вводим значение "y" в строку.
-    input1 = browser.find_element(By.ID, "answer")
-    input1.send_keys(y)
+        x_element = browser.find_element(By.CSS_SELECTOR, "[valuex]")
+        x = x_element.get_attribute("valuex")
+        y = calc(x)
 
-    # выбираем чекбокс - im the Robot
-    option1 = browser.find_element(By.ID, "robotCheckbox")
-    option1.click()
-
-    # выбираем радиобатон - Robots rule
-    option2 = browser.find_element(By.ID, "robotsRule")
-    option2.click()
-
-    # жмем Submit
-    option3 = browser.find_element(By.CSS_SELECTOR, ".btn")
-    option3.click()
-
-finally:
-    # ожидание чтобы визуально оценить результаты прохождения скрипта
-    time.sleep(10)
-    # закрываем браузер после всех манипуляций
-    browser.quit()
+        browser.find_element(By.ID, "answer").send_keys(y)
+        browser.find_element(By.ID, "robotCheckbox").click()
+        browser.find_element(By.ID, "robotsRule").click()
+        browser.find_element(By.CSS_SELECTOR, ".btn").click()
+    finally:
+        time.sleep(5)
