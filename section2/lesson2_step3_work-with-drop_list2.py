8a20b2d82f8a6a44c7d8c50a850237c337cb0ca8
goat <customlinuxqa@gmail.com>
2022-12-06 18:30:37 +0300

    refactoring

--- section2/lesson2_step3_work-with-drop_list2.py
+++ section2/lesson2_step3_work-with-drop_list2.py
@@ -1,44 +1,25 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
+import time
+import chromedriver_autoinstaller
 
-import time, math
-
-try:
-    link = "http://suninjuly.github.io/selects2.html"
-    browser = webdriver.Chrome()
-    browser.get(link)
+chromedriver_autoinstaller.install()
+link = "http://suninjuly.github.io/selects2.html"
 
 
-    # Создаем функцию calc(x)
-    def calc(x):
-        return str(num1 + num2)
+def test_fill_form():
+    try:
+        browser = webdriver.Chrome()
+        browser.get(link)
 
+        num1 = browser.find_element(By.CSS_SELECTOR, "h2 span:nth-child(2)").text
+        num2 = browser.find_element(By.CSS_SELECTOR, "h2 span:nth-child(4)").text
 
-    # Находим элементы
-    id_num1 = browser.find_element(By.CSS_SELECTOR, "h2 span:nth-child(2)")
-    id_num2 = browser.find_element(By.CSS_SELECTOR, "h2 span:nth-child(4)")
+        sum = int(num1) + int(num2)
 
-    # Определяем значение строки
-    num1 = id_num1.text
-    num2 = id_num2.text
+        Select(browser.find_element(By.ID, "dropdown")).select_by_value(str(sum))
+        browser.find_element(By.CSS_SELECTOR, ".btn").click()
 
-    # Посчитаем сумму
-    sum = int(num1) + int(num2)
-    print(sum)
-    # option1 = browser.find_element(By.ID, "dropdown")
-    # option1.click()
-
-    select = Select(browser.find_element(By.ID, "dropdown"))
-    # print()
-    select.select_by_value(str(sum))
-
-    # жмем Submit
-    option2 = browser.find_element(By.CSS_SELECTOR, ".btn")
-    option2.click()
-
-finally:
-    # ожидание чтобы визуально оценить результаты прохождения скрипта
-    time.sleep(10)
-    # закрываем браузер после всех манипуляций
-    browser.quit()
+    finally:
+        time.sleep(5)
