647aa7948a266ad9b39b57966f7ec5a667e8aeb5
goat <customlinuxqa@gmail.com>
2022-12-06 18:46:12 +0300

    refactoring

--- section2/lesson2_step6_execute_script.py
+++ section2/lesson2_step6_execute_script.py
@@ -1,50 +1,40 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-import time, math
+import time
+import math
+import chromedriver_autoinstaller
 
-try:
-    link = "http://suninjuly.github.io/execute_script.html"
-    browser = webdriver.Chrome()
-    browser.get(link)
-
-
-    # Создаем функцию calc(x)
-    def calc(x):
-        return str(math.log(abs(12 * math.sin(int(x)))))
+chromedriver_autoinstaller.install()
+link = "http://suninjuly.github.io/execute_script.html"
 
 
-    # Находим элемент Х
-    x_element = browser.find_element(By.CSS_SELECTOR, ".form-group [id=input_value]")
-    x = x_element.text
-    y = calc(x)
+def calc(x):
+    return str(math.log(abs(12 * math.sin(int(x)))))
 
-    answer_string = browser.find_element(By.ID, "answer")
-    browser.execute_script("return arguments[0].scrollIntoView(true);", answer_string)
 
-    # вводим значение "y" в строку.
-    input1 = browser.find_element(By.ID, "answer")
-    input1.send_keys(y)
+def test_fill_form():
+    try:
+        browser = webdriver.Chrome()
+        browser.get(link)
 
-    check_box = browser.find_element(By.ID, "robotCheckbox")
-    browser.execute_script("return arguments[0].scrollIntoView(true);", check_box)
+        x = browser.find_element(By.CSS_SELECTOR, ".form-group [id=input_value]").text
+        y = calc(x)
 
-    # выбираем чекбокс - im the Robot
-    option1 = browser.find_element(By.ID, "robotCheckbox")
-    option1.click()
+        answer_string = browser.find_element(By.ID, "answer")
+        browser.execute_script("return arguments[0].scrollIntoView(true);", answer_string)
+        browser.find_element(By.ID, "answer").send_keys(y)
 
-    radio_button = browser.find_element(By.ID, "robotsRule")
-    browser.execute_script("return arguments[0].scrollIntoView(true);", radio_button)
+        check_box = browser.find_element(By.ID, "robotCheckbox")
+        browser.execute_script("return arguments[0].scrollIntoView(true);", check_box)
+        browser.find_element(By.ID, "robotCheckbox").click()
 
-    # выбираем радиобатон - Robots rule
-    option2 = browser.find_element(By.ID, "robotsRule")
-    option2.click()
+        radio_button = browser.find_element(By.ID, "robotsRule")
+        browser.execute_script("return arguments[0].scrollIntoView(true);", radio_button)
+        browser.find_element(By.ID, "robotsRule").click()
 
-    button = browser.find_element(By.CSS_SELECTOR, "button")
-    browser.execute_script("return arguments[0].scrollIntoView(true);", button)
-    button.click()
+        button = browser.find_element(By.CSS_SELECTOR, "button")
+        browser.execute_script("return arguments[0].scrollIntoView(true);", button)
+        button.click()
 
-finally:
-    # ожидание чтобы визуально оценить результаты прохождения скрипта
-    time.sleep(10)
-    # закрываем браузер после всех манипуляций
-    browser.quit()
+    finally:
+        time.sleep(5)
